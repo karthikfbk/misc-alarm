c7f77f93ba58abbd2e5af8c8ca51842e2d5cc307
karthik <kar.fbk@gmail.com>
2014-03-13 23:52:18 +0530

    new action bar changes

--- gen/com/example/inicus/R.java
+++ gen/com/example/inicus/R.java
@@ -21,9 +21,41 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_action_new=0x7f020000;
-        public static final int ic_action_previous_item=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
+        public static final int ab_background_textured_example=0x7f020000;
+        public static final int ab_bottom_solid_example=0x7f020001;
+        public static final int ab_solid_example=0x7f020002;
+        public static final int ab_stacked_solid_example=0x7f020003;
+        public static final int ab_texture_tile_example=0x7f020004;
+        public static final int ab_transparent_example=0x7f020005;
+        public static final int btn_cab_done_default_example=0x7f020006;
+        public static final int btn_cab_done_example=0x7f020007;
+        public static final int btn_cab_done_focused_example=0x7f020008;
+        public static final int btn_cab_done_pressed_example=0x7f020009;
+        public static final int cab_background_bottom_example=0x7f02000a;
+        public static final int cab_background_top_example=0x7f02000b;
+        public static final int ic_action_new=0x7f02000c;
+        public static final int ic_action_previous_item=0x7f02000d;
+        public static final int ic_launcher=0x7f02000e;
+        public static final int list_focused_example=0x7f02000f;
+        public static final int list_pressed_example=0x7f020010;
+        public static final int menu_dropdown_panel_example=0x7f020011;
+        public static final int progress_bg_example=0x7f020012;
+        public static final int progress_horizontal_example=0x7f020013;
+        public static final int progress_primary_example=0x7f020014;
+        public static final int progress_secondary_example=0x7f020015;
+        public static final int selectable_background_example=0x7f020016;
+        public static final int spinner_ab_default_example=0x7f020017;
+        public static final int spinner_ab_disabled_example=0x7f020018;
+        public static final int spinner_ab_focused_example=0x7f020019;
+        public static final int spinner_ab_pressed_example=0x7f02001a;
+        public static final int spinner_background_ab_example=0x7f02001b;
+        public static final int tab_indicator_ab_example=0x7f02001c;
+        public static final int tab_selected_example=0x7f02001d;
+        public static final int tab_selected_focused_example=0x7f02001e;
+        public static final int tab_selected_pressed_example=0x7f02001f;
+        public static final int tab_unselected_example=0x7f020020;
+        public static final int tab_unselected_focused_example=0x7f020021;
+        public static final int tab_unselected_pressed_example=0x7f020022;
     }
     public static final class id {
         public static final int add_event=0x7f080003;
@@ -53,6 +85,10 @@ public final class R {
         public static final int title_activity_home=0x7f050005;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Example=0x7f060003;
+        public static final int ActionBar_Transparent_Example=0x7f060004;
+        public static final int ActionBarTabStyle_Example=0x7f060007;
+        public static final int ActionButton_CloseMode_Example=0x7f06000a;
         /** 
         Base application theme, dependent on API level. This theme is replaced
         by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
@@ -79,5 +115,15 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int DropDownListView_Example=0x7f060006;
+        public static final int DropDownNav_Example=0x7f060008;
+        public static final int PopupMenu_Example=0x7f060005;
+        public static final int ProgressBar_Example=0x7f060009;
+        public static final int Theme_Example=0x7f060002;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
+ this style is only referenced in a Light.DarkActionBar based theme 
+ this style is only referenced in a Light.DarkActionBar based theme 
+         */
+        public static final int Theme_Example_Widget=0x7f06000b;
     }
 }
