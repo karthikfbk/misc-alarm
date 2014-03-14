9728493ab7313c7ad8c189f82153c3eac1dae6e8
karthik <kar.fbk@gmail.com>
2014-03-15 00:01:54 +0530

    commit on 15_03_14

--- src/com/example/inicus/Home.java
+++ src/com/example/inicus/Home.java
@@ -1,10 +1,15 @@
 package com.example.inicus;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
+//import android.support.v4.view.MenuItemCompat;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class Home extends Activity {
 
@@ -14,10 +19,17 @@ public class Home extends Activity {
 		setContentView(R.layout.activity_home);
 	}
 
+	@SuppressLint("NewApi")
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.home, menu);    
+
+		
+	    SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = new SearchView(getActionBar().getThemedContext());
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        menu.findItem(R.id.search_event).setActionView(searchView);
 		return true;
 	}
 	
@@ -26,15 +38,23 @@ public class Home extends Activity {
 	    // Handle presses on the action bar items
 	    switch (item.getItemId()) {
 	        case R.id.add_event:
-	            NavigateToAddEvent();
-	            return true;	        
+	           NavigateToAddEvent();
+	           return true;
+	        case R.id.search_event:
+	        Displaysearch();
+	        
 	        default:
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
 
-	private void NavigateToAddEvent() {
+	private void Displaysearch() {
 		// TODO Auto-generated method stub
+		//Do nothing
+	}
+
+	private void NavigateToAddEvent() {
+		//TODO Auto-generated method stub
 		Intent intent = new Intent(this, Add_Event.class);
 		startActivity(intent);
 	}
